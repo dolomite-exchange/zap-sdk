09cea930eb6301841cd55185ac2279a4ca45530d
Corey <corey@dolomite.io>
2023-08-21 10:48:20 -0400

    v0.1.21

---
+++ __tests__/specific-zaps/PendleYtZap.test.ts
@@ -0,0 +1,92 @@
+import Deployments from '@dolomite-exchange/dolomite-margin-modules/scripts/deployments.json';
+import BigNumber from 'bignumber.js';
+import { ethers } from 'ethers';
+import { DolomiteZap, GenericTraderType, Network } from '../../src';
+import { PT_GLP_MARKET, USDC_MARKET } from '../helpers/TestConstants';
+
+const txOrigin = '0x52256ef863a713Ef349ae6E97A7E8f35785145dE';
+
+describe('DolomiteZap', () => {
+  const network = Network.ARBITRUM_ONE;
+  const subgraphUrl = process.env.SUBGRAPH_URL;
+  if (!subgraphUrl) {
+    throw new Error('SUBGRAPH_URL env var not set');
+  }
+  const web3Provider = new ethers.providers.JsonRpcProvider(process.env.WEB3_PROVIDER_URL);
+  const NO_CACHE = -1;
+  const zap = new DolomiteZap(network, subgraphUrl, web3Provider, NO_CACHE);
+
+  describe('#getSwapExactTokensForTokensData', () => {
+    describe('Pendle tokens', () => {
+      it('should work when unwrapping a PT-token', async () => {
+        const amountIn = new BigNumber('100000000000000000000'); // 100 PT
+        const minAmountOut = new BigNumber('50000000'); // 50 USDC
+        const outputParams = await zap.getSwapExactTokensForTokensParams(
+          PT_GLP_MARKET,
+          amountIn,
+          USDC_MARKET,
+          minAmountOut,
+          txOrigin,
+        );
+
+        expect(outputParams.length).toBe(1);
+
+        const outputParam = outputParams[0];
+        expect(outputParam.marketIdsPath.length).toEqual(2);
+        expect(outputParam.marketIdsPath[0]).toEqual(PT_GLP_MARKET.marketId);
+        expect(outputParam.marketIdsPath[1]).toEqual(USDC_MARKET.marketId);
+
+        expect(outputParam.amountWeisPath.length).toEqual(2);
+        expect(outputParam.amountWeisPath[0]).toEqual(amountIn);
+        expect(outputParam.amountWeisPath[1].isGreaterThan(minAmountOut)).toBeTruthy();
+
+        expect(outputParam.traderParams.length).toEqual(1);
+        expect(outputParam.traderParams[0].traderType).toEqual(GenericTraderType.IsolationModeUnwrapper);
+        expect(outputParam.traderParams[0].makerAccountIndex).toEqual(0);
+        expect(outputParam.traderParams[0].trader)
+          .toEqual(Deployments.PendlePtGLP2024IsolationModeUnwrapperTraderV2[network].address);
+        expect(outputParam.traderParams[0].tradeData.length).toBeGreaterThan(66);
+
+        expect(outputParam.makerAccounts.length).toEqual(0);
+        expect(outputParam.expectedAmountOut.gt(outputParam.amountWeisPath[outputParam.amountWeisPath.length - 1]))
+          .toBeTruthy();
+        expect(outputParam.originalAmountOutMin).toEqual(minAmountOut);
+      });
+
+      it('should work when wrapping a PT-token', async () => {
+        const amountIn = new BigNumber('100000000'); // 100 USDC
+        const minAmountOut = new BigNumber('50000000000000000000'); // 50 PT
+        const outputParams = await zap.getSwapExactTokensForTokensParams(
+          USDC_MARKET,
+          amountIn,
+          PT_GLP_MARKET,
+          minAmountOut,
+          txOrigin,
+        );
+
+        expect(outputParams.length).toBe(1);
+
+        const outputParam = outputParams[0];
+        expect(outputParam.marketIdsPath.length).toEqual(2);
+        expect(outputParam.marketIdsPath[0]).toEqual(USDC_MARKET.marketId);
+        expect(outputParam.marketIdsPath[1]).toEqual(PT_GLP_MARKET.marketId);
+
+        expect(outputParam.amountWeisPath.length).toEqual(2);
+        expect(outputParam.amountWeisPath[0]).toEqual(amountIn);
+        expect(outputParam.amountWeisPath[1].isGreaterThan(minAmountOut)).toBeTruthy();
+
+        expect(outputParam.traderParams.length).toEqual(1);
+        expect(outputParam.traderParams[0].traderType).toEqual(GenericTraderType.IsolationModeWrapper);
+        expect(outputParam.traderParams[0].makerAccountIndex).toEqual(0);
+        expect(outputParam.traderParams[0].trader)
+          .toEqual(Deployments.PendlePtGLP2024IsolationModeWrapperTraderV2[network].address);
+        expect(outputParam.traderParams[0].tradeData.length).toBeGreaterThan(66);
+
+        expect(outputParam.makerAccounts.length).toEqual(0);
+        expect(outputParam.expectedAmountOut.gt(outputParam.amountWeisPath[outputParam.amountWeisPath.length - 1]))
+          .toBeTruthy();
+        expect(outputParam.originalAmountOutMin).toEqual(minAmountOut);
+      });
+    });
+  });
+});
---
+++ src/lib/estimators/PendleYtEstimator.ts
@@ -0,0 +1,109 @@
+import { BaseRouter as PendleRouter, Router as PendleStaticRouter } from '@pendle/sdk-v2';
+import BigNumber from 'bignumber.js';
+import { ethers } from 'ethers';
+import { Address, Integer, Network } from '../ApiTypes';
+import { getPendleMarketForIsolationModeToken, getSGlpAddress } from '../Constants';
+
+export class PendlePtEstimator {
+  private readonly network: Network;
+  private pendleRouter: PendleRouter;
+
+  public constructor(
+    network: Network,
+    web3Provider: ethers.providers.Provider,
+  ) {
+    this.network = network;
+    this.pendleRouter = PendleStaticRouter.getRouter({
+      chainId: network as any,
+      provider: web3Provider,
+      signer: new ethers.VoidSigner('0x1234567812345678123456781234567812345678', web3Provider),
+    });
+  }
+
+  public set web3Provider(web3Provider: ethers.providers.Provider) {
+    this.pendleRouter = PendleStaticRouter.getRouter({
+      chainId: this.network as any,
+      provider: web3Provider,
+      signer: new ethers.VoidSigner('0x1234567812345678123456781234567812345678', web3Provider),
+    });
+  }
+
+  public async getUnwrappedAmount(
+    isolationModeToken: Address,
+    amountInPt: Integer,
+  ): Promise<{ tradeData: string; outputAmount: Integer }> {
+    const [, , , tokenOutput] = await this.pendleRouter.swapExactPtForToken(
+      getPendleMarketForIsolationModeToken(this.network, isolationModeToken) as any,
+      amountInPt.toFixed(),
+      getSGlpAddress(this.network) as any,
+      0,
+      { method: 'extractParams' },
+    );
+
+    const tradeData = ethers.utils.defaultAbiCoder.encode(
+      ['tuple(address,uint256,address,address,address,tuple(uint8,address,bytes,bool))'],
+      [
+        [
+          tokenOutput.tokenOut,
+          tokenOutput.minTokenOut,
+          tokenOutput.tokenRedeemSy,
+          tokenOutput.bulk,
+          tokenOutput.pendleSwap,
+          [
+            tokenOutput.swapData.swapType,
+            tokenOutput.swapData.extRouter,
+            tokenOutput.swapData.extCalldata,
+            tokenOutput.swapData.needScale,
+          ],
+        ],
+      ],
+    );
+
+    const outputAmount = new BigNumber(tokenOutput.minTokenOut.toString());
+    return { tradeData, outputAmount };
+  }
+
+  public async getWrappedAmount(
+    isolationModeToken: Address,
+    inputAmount: Integer,
+  ): Promise<{ tradeData: string; ptAmountOut: Integer }> {
+    const [, , , approxParams, tokenInput] = await this.pendleRouter.swapExactTokenForPt(
+      getPendleMarketForIsolationModeToken(this.network, isolationModeToken) as any,
+      getSGlpAddress(this.network)?.toLowerCase() as any,
+      inputAmount.toFixed(),
+      0,
+      { method: 'extractParams' },
+    );
+
+    const approxParamsType = 'tuple(uint256,uint256,uint256,uint256,uint256)';
+    const tokenInputType = 'tuple(address,uint256,address,address,address,tuple(uint8,address,bytes,bool))';
+    const tradeData = ethers.utils.defaultAbiCoder.encode(
+      [approxParamsType, tokenInputType],
+      [
+        [
+          approxParams.guessMin,
+          approxParams.guessMax,
+          approxParams.guessOffchain,
+          approxParams.maxIteration,
+          approxParams.eps,
+        ],
+        [
+          tokenInput.tokenIn,
+          tokenInput.netTokenIn,
+          tokenInput.tokenMintSy,
+          tokenInput.bulk,
+          tokenInput.pendleSwap,
+          [
+            tokenInput.swapData.swapType,
+            tokenInput.swapData.extRouter,
+            tokenInput.swapData.extCalldata,
+            tokenInput.swapData.needScale,
+          ],
+        ],
+      ],
+    );
+
+    const ptAmountOut = new BigNumber(approxParams.guessOffchain.toString());
+    return { tradeData, ptAmountOut };
+  }
+}
