fbf1500a15a0d1b8bc24fb18373a0c4986bfb1d5
Corey <corey@dolomite.io>
2023-12-17 23:28:13 -0500

    v0.1.32

---
+++ src/lib/estimators/SimpleEstimator.ts
@@ -0,0 +1,111 @@
+import { BaseRouter as PendleRouter, Router as PendleStaticRouter } from '@pendle/sdk-v2';
+import BigNumber from 'bignumber.js';
+import { ethers } from 'ethers';
+import { Address, EstimateOutputResult, Integer, Network } from '../ApiTypes';
+import { getPendleMarketForIsolationModeToken } from '../Constants';
+
+export class PendlePtEstimator {
+  private readonly network: Network;
+  private pendleRouter: PendleRouter;
+
+  public constructor(
+    network: Network,
+    web3Provider: ethers.providers.Provider,
+  ) {
+    this.network = network;
+    this.pendleRouter = PendleStaticRouter.getRouter({
+      chainId: network as any,
+      provider: web3Provider,
+      signer: new ethers.VoidSigner('0x1234567812345678123456781234567812345678', web3Provider),
+    });
+  }
+
+  public set web3Provider(web3Provider: ethers.providers.Provider) {
+    this.pendleRouter = PendleStaticRouter.getRouter({
+      chainId: this.network as any,
+      provider: web3Provider,
+      signer: new ethers.VoidSigner('0x1234567812345678123456781234567812345678', web3Provider),
+    });
+  }
+
+  public async getUnwrappedAmount(
+    isolationModeToken: Address,
+    amountInPt: Integer,
+    tokenOut: Address,
+  ): Promise<EstimateOutputResult> {
+    const [, , , tokenOutput] = await this.pendleRouter.swapExactPtForToken(
+      getPendleMarketForIsolationModeToken(this.network, isolationModeToken) as any,
+      amountInPt.toFixed(),
+      tokenOut as any,
+      0,
+      { method: 'extractParams' },
+    );
+
+    const tradeData = ethers.utils.defaultAbiCoder.encode(
+      ['tuple(address,uint256,address,address,address,tuple(uint8,address,bytes,bool))'],
+      [
+        [
+          tokenOutput.tokenOut,
+          /* tokenOutput.minTokenOut = */ '1',
+          tokenOutput.tokenRedeemSy,
+          tokenOutput.bulk,
+          tokenOutput.pendleSwap,
+          [
+            tokenOutput.swapData.swapType,
+            tokenOutput.swapData.extRouter,
+            tokenOutput.swapData.extCalldata,
+            tokenOutput.swapData.needScale,
+          ],
+        ],
+      ],
+    );
+
+    const amountOut = new BigNumber(tokenOutput.minTokenOut.toString());
+    return { tradeData, amountOut };
+  }
+
+  public async getWrappedAmount(
+    isolationModeToken: Address,
+    inputAmount: Integer,
+    inputToken: Address,
+  ): Promise<EstimateOutputResult> {
+    const [, , , approxParams, tokenInput] = await this.pendleRouter.swapExactTokenForPt(
+      getPendleMarketForIsolationModeToken(this.network, isolationModeToken) as any,
+      inputToken as any,
+      inputAmount.toFixed(),
+      0,
+      { method: 'extractParams' },
+    );
+
+    const approxParamsType = 'tuple(uint256,uint256,uint256,uint256,uint256)';
+    const tokenInputType = 'tuple(address,uint256,address,address,address,tuple(uint8,address,bytes,bool))';
+    const tradeData = ethers.utils.defaultAbiCoder.encode(
+      [approxParamsType, tokenInputType],
+      [
+        [
+          approxParams.guessMin,
+          approxParams.guessMax,
+          approxParams.guessOffchain,
+          approxParams.maxIteration,
+          approxParams.eps,
+        ],
+        [
+          tokenInput.tokenIn,
+          tokenInput.netTokenIn,
+          tokenInput.tokenMintSy,
+          tokenInput.bulk,
+          tokenInput.pendleSwap,
+          [
+            tokenInput.swapData.swapType,
+            tokenInput.swapData.extRouter,
+            tokenInput.swapData.extCalldata,
+            tokenInput.swapData.needScale,
+          ],
+        ],
+      ],
+    );
+
+    const amountOut = new BigNumber(approxParams.guessOffchain.toString());
+    return { tradeData, amountOut };
+  }
+}
