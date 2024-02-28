b887221a17cf5b88568f85756f106ba9097db2f7
Corey Caplan <coreycaplan3@gmail.com>
2024-02-28 11:41:12 -0700

    fixed gmx v2 zap testing

--- __tests__/specific-zaps/GmxV2Zap.test.ts
+++ __tests__/specific-zaps/GmxV2Zap.test.ts
@@ -2,7 +2,14 @@ import BigNumber from 'bignumber.js';
 import { ethers } from 'ethers';
 import { parseEther } from 'ethers/lib/utils';
 import { DolomiteZap, GenericTraderType, Network } from '../../src';
-import { GM_ARB_MARKET, GM_BTC_MARKET, GM_ETH_MARKET, GM_LINK_MARKET, USDC_MARKET } from '../helpers/TestConstants';
+import {
+  GM_ARB_MARKET,
+  GM_BTC_MARKET,
+  GM_ETH_MARKET,
+  GM_LINK_MARKET,
+  NATIVE_USDC_MARKET,
+  USDC_MARKET,
+} from '../helpers/TestConstants';
 
 const txOrigin = '0x52256ef863a713Ef349ae6E97A7E8f35785145dE';
 
@@ -46,18 +53,19 @@ describe('GmxV2Zap', () => {
           const outputParam = outputParams[0];
           expect(outputParam.marketIdsPath.length).toEqual(3);
           expect(outputParam.marketIdsPath[0]).toEqual(market.marketId);
-          expect(outputParam.marketIdsPath[1]).toEqual(USDC_MARKET.marketId);
+          expect(outputParam.marketIdsPath[1]).toEqual(NATIVE_USDC_MARKET.marketId);
+          expect(outputParam.marketIdsPath[2]).toEqual(USDC_MARKET.marketId);
 
-          expect(outputParam.amountWeisPath.length).toEqual(2);
+          expect(outputParam.amountWeisPath.length).toEqual(3);
           expect(outputParam.amountWeisPath[0]).toEqual(amountIn);
-          expect(outputParam.amountWeisPath[1].isGreaterThan(minAmountOut)).toBeTruthy();
+          expect(outputParam.amountWeisPath[2].isGreaterThan(minAmountOut)).toBeTruthy();
 
-          expect(outputParam.traderParams.length).toEqual(1);
+          expect(outputParam.traderParams.length).toEqual(2);
           expect(outputParam.traderParams[0].traderType).toEqual(GenericTraderType.IsolationModeUnwrapper);
           expect(outputParam.traderParams[0].makerAccountIndex).toEqual(0);
           expect(outputParam.traderParams[0].trader)
             .toEqual(market.isolationModeUnwrapperInfo!.unwrapperAddress);
-          expect(outputParam.traderParams[0].tradeData.length).toBeGreaterThan(66);
+          expect(outputParam.traderParams[0].tradeData.length).toEqual(66);
 
           expect(outputParam.makerAccounts.length).toEqual(0);
           expect(outputParam.expectedAmountOut.gt(outputParam.amountWeisPath[outputParam.amountWeisPath.length - 1]))
@@ -84,18 +92,19 @@ describe('GmxV2Zap', () => {
           const outputParam = outputParams[0];
           expect(outputParam.marketIdsPath.length).toEqual(3);
           expect(outputParam.marketIdsPath[0]).toEqual(USDC_MARKET.marketId);
-          expect(outputParam.marketIdsPath[1]).toEqual(market.marketId);
+          expect(outputParam.marketIdsPath[1]).toEqual(NATIVE_USDC_MARKET.marketId);
+          expect(outputParam.marketIdsPath[2]).toEqual(market.marketId);
 
-          expect(outputParam.amountWeisPath.length).toEqual(2);
+          expect(outputParam.amountWeisPath.length).toEqual(3);
           expect(outputParam.amountWeisPath[0]).toEqual(amountIn);
-          expect(outputParam.amountWeisPath[1].isGreaterThan(minAmountOut)).toBeTruthy();
+          expect(outputParam.amountWeisPath[2].isGreaterThan(minAmountOut)).toBeTruthy();
 
-          expect(outputParam.traderParams.length).toEqual(1);
-          expect(outputParam.traderParams[0].traderType).toEqual(GenericTraderType.IsolationModeWrapper);
-          expect(outputParam.traderParams[0].makerAccountIndex).toEqual(0);
-          expect(outputParam.traderParams[0].trader)
+          expect(outputParam.traderParams.length).toEqual(2);
+          expect(outputParam.traderParams[1].traderType).toEqual(GenericTraderType.IsolationModeWrapper);
+          expect(outputParam.traderParams[1].makerAccountIndex).toEqual(0);
+          expect(outputParam.traderParams[1].trader)
             .toEqual(market.isolationModeWrapperInfo?.wrapperAddress);
-          expect(outputParam.traderParams[0].tradeData.length).toBeGreaterThan(66);
+          expect(outputParam.traderParams[1].tradeData.length).toEqual(66);
 
           expect(outputParam.makerAccounts.length).toEqual(0);
           expect(outputParam.expectedAmountOut.gt(outputParam.amountWeisPath[outputParam.amountWeisPath.length - 1]))
