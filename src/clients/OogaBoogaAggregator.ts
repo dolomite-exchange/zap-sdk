44dc613e36ce3efc30650eadd15a487e8fddbd51
scyron6 <stephencyron@gmail.com>
2025-09-18 13:53:12 -0400

    feat: liquidity source blacklist

--- src/clients/OogaBoogaAggregator.ts
+++ src/clients/OogaBoogaAggregator.ts
@@ -48,6 +48,7 @@ export default class OogaBoogaAggregator extends AggregatorClient {
       amount: inputAmountWei.toFixed(),
       to: traderAddress,
       slippage: zapConfig.slippageTolerance.toString(),
+      liquiditySourcesBlacklist: 'Burve',
       excludeDolomiteTokens: 'true',
     });
     const quoteResponse: any | Error = await AxiosClient.get(
